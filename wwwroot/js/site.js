0f4677469af5a5ab9cc8f9393074f20b51ea537e
Kik099 <71726341+Kik099@users.noreply.github.com>
2021-05-06 03:58:25 +0100

    Add files via upload

---
+++ wwwroot/js/site.js
@@ -0,0 +1,36 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+jQueryAjaxDelete = form => {
+    if (confirm('Tem a certeza que pretende eliminar ?')) {
+
+
+        try {
+            $.ajax({
+
+                type: 'POST',
+                url: form.action,
+                success: function (res) {
+                    $("#").html(res.html);
+                },
+                error: function (err) {
+                    console.log(err);
+                }
+
+            })
+
+        }
+        catch (e) {
+            console.log(e); 
+        }
+
+
+
+
+
+    }
+    return false;
+
+}
